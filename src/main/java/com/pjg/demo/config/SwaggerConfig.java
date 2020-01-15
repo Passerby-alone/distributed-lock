2d637c3b1a7264e7a9cf785da6da2c80c7415b9c
pengjinguo <houyan.peng@ezhiyang.com>
2020-01-15 13:42:37 +0800

    修复git提交

--- src/main/java/com/pjg/demo/config/SwaggerConfig.java
+++ src/main/java/com/pjg/demo/config/SwaggerConfig.java
@@ -2,10 +2,8 @@ package com.pjg.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -43,17 +41,12 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
+
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-                // addPathPatterns 用于添加拦截规则 ， 先把所有路径都加入拦截， 再一个个排除
-        registry.addInterceptor(new LocaleChangeInterceptor()).addPathPatterns("/**")
-                .excludePathPatterns("/swagger-ui.html")
-                .excludePathPatterns("/swagger-resources/**")
-                .excludePathPatterns("/error")
-                .excludePathPatterns("/webjars/**");
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/resources/")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/public/");
     }
 }
